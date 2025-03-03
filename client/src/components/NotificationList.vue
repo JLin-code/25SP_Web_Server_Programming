a0fa918b3b3b0d73251040dab8f9dc79187753aa
Jack Lin <lin_jack@myyahoo.com>
2025-03-03 15:03:13 -0500

    errors

---
+++ client/src/components/NotificationList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const notifications = ref([
+    { text: 'This is a primary notification #1', type: 'primary' },
+    { text: 'This is a primary notification #2', type: 'primary' },
+    { text: 'This is a primary notification #3', type: 'primary' },
+    { text: 'This is a primary notification #4', type: 'primary' },
+]);
+
+function removeNotification(index: number) {
+    notifications.value.splice(index, 1);
+};
+
+</script>
+
+<template>
+    <div>
+        <div v-for="(message, index) in notifications" :key="index" class="notification is-success is-light">
+                            <button class="delete" @click="removeNotification(index)"></button>
+                            {{ message }}
+                        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
