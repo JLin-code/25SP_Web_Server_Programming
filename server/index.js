52f8bf9aa1b708bd987304b9d5bbb336c4eccf55
Jack Lin <lin_jack@myyahoo.com>
2025-04-07 13:58:58 -0400

    Moved file location and changed config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 // Load the http module to create an http server.
 const express = require('express')
 const productsController = require('./controllers/products')
-const productsController = require('./controllers/products');
 const e = require('express');
 
 const PORT = 8000
@@ -9,14 +8,17 @@ const PORT = 8000
 const app = express();
 
 // Middleware
-    app.use(express.json()) // Parse JSON request body
-//controller middleware
+    app.use(express.json())
 
+
+//Controllers
 app
-  .get('/', (req, res) => {
+  .get('/hello', (req, res) => {
     res.send('Hello New Paltz, NY!!!')
   })
   .use('/api/v1/products', productsController)
+
+  .use('/', express.static('dist'))
 
 
 //error handling middleware
